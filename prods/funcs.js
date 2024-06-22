c919f3aa0e6cbbfe06e983cd51948244cb80dddd
Anıl <142122890+anildombayci@users.noreply.github.com>
2024-06-22 16:22:52 +0300

    Update funcs.js

--- prods/funcs.js
+++ prods/funcs.js
@@ -13,6 +13,20 @@ module.exports = {
   add: async (name, barcode, price, stock) => {
     db.push("urunler", { name: name, barcode: barcode, price: price, stock: stock })
     return { name: name, barcode: barcode, price: price, stock: stock }
-  },  
+  },
+  downOfStock: async (barcode, count) => {
+  let products = db.get('urunler');
+  const productIndex = products.findIndex(urun => urun.barcode === barcode);
+  if (productIndex !== -1) {
+    const productName = products[productIndex].name;
+    const previousStock = products[productIndex].stock;
+    products[productIndex].stock -= count;
+    db.set('urunler', products);
+    const message = `Stok azaltma işlemi başarılı: ${count} adet "${productName}" ürününden düşürüldü. Stok: ${previousStock} ➔ ${products[productIndex].stock}`;
+    return { status: 200, message };
+  } else {
+    return { status: 404, message: 'Böyle bir barkod numarasına sahip ürün bulunamadı.' };
+  }
+  },
   db: db
 }
